7a112c944cc5ec1e0c09c7969ba5c74b156fa03d
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2024-06-06 10:04:55 +0530

    commit

---
+++ Module5/springrestapi-1/src/main/java/com/example/demo/model/Student.java
@@ -0,0 +1,51 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "abhishek_info")
+public class Student 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	int id;
+	@Column(name = "name")
+	String name;
+	@Column(name = "email")
+	String email;
+	@Column(name = "phone")
+	String phone;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
+}
