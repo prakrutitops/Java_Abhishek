6ad7345a7f92617c59f5059a94a02a62c3119fb3
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2024-06-08 10:03:32 +0530

    commit

---
+++ Module5/springsecurity/src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableWebSecurity
+public class StudentController 
+{
+	
+	
+	@RequestMapping("/home")
+	public String index()
+	{
+		return "Welcome to tops";
+	}
+	
+	
+	
+}
